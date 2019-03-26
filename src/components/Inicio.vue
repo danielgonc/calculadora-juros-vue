5a2d57a42ce18f17f13d59e812119464a13b828c
Daniel dos Santos <40795649+danielgonc@users.noreply.github.com>
2019-03-26 14:03:34 -0400

    commit inicial

---
+++ src/components/Inicio.vue
@@ -0,0 +1,75 @@
+<template>
+    <div id="app">
+        <div class="esquerda">
+            <h1>{{nomeForm}}</h1>
+            <form v-on:submit="submitForm">
+                <label for="jurosMesEle" class="label-padrao">Juros por mes</label>
+                <input id="jurosMesEle" v-model="jurosMes" type="number" class="input-padrao">
+                <label for="multaAtrasoEle" class="label-padrao">Multa por atraso</label>
+                <input id="multaAtrasoEle" v-model="multaAtraso" type="number" class="input-padrao">
+                <label for="valorMenEle" class="label-padrao">Valor da mensalidade</label>
+                <input id="valorMenEle" v-model="valorMen" type="number" class="input-padrao">
+                <label for="multaAtrasoEle" class="label-padrao">Data de vencimento</label>
+                <input id="multaAtrasoEle" v-model="multaAtraso" type="date" class="input-padrao">
+                <button class="btn-submeter">Enviar</button>
+            </form>
+        </div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name: "app",
+    data: function(){
+        return {
+            nomeForm: "Dados Iniciais",
+            
+        }
+    },
+    computed:{
+        jurosMes: {get(){ return this.value}, set(val){ this.value = val}},
+        multaAtraso: {get(){ return this.value}, set(val){ this.value = val}},
+        valorMen: {get(){ return this.value}, set(val){ this.value = val}},
+        dataHoje: {get(){ return this.value}, set(val){ this.value = val}},
+        dataVencimento: {get(){ return this.value}, set(val){ this.value = val}},
+    },
+    methods: {
+        submitForm: (e) =>{
+            e.preventDefault()
+            alert(jurosMes);
+        }
+    }
+}
+</script>
+
+<style>
+.esquerda{
+    width: 50vw;
+    float: left; 
+}
+.label-padrao{
+    width: 50%;
+    display: inline-block;
+
+}
+.input-padrao{
+    width: 50%;
+    display: inline-block;
+    height: 24px;
+
+}
+.btn-submeter{
+    width: 50%;
+    height: 35px;;
+    margin-top: 25px;
+    background-color: #000;
+    border: none;
+    border-radius: 4px;
+    color: #fff;
+    cursor: pointer;
+    transition: all 0.3s;
+}
+.btn-submeter:hover{
+    background-color: #888
+}
+</style>
